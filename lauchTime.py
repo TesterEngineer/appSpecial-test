8fba6e838a36a687db7a607d0766c3aaa09b124b
TesterEngineer <1032241157@qq.com>
2018-09-21 18:00:40 +0800

    启动时间的adb测试

---
+++ lauchTime.py
@@ -0,0 +1,42 @@
+#-*- coding:utf-8 _*-
+"""
+@author:Duan jun ming
+@file: lauchTime.py
+@time: 2018/09/21
+qq:1032241157
+"""
+import os
+
+
+class app(object):
+
+    def __init__(self):
+        self.data = ""
+        self.content =""
+
+
+    def get_appLaunchTime(self):
+        #冷启动的adb命令
+        cmd="adb shell am start -W -n  	com.woqutz.didi/com.ryg.dynamicload.DLProxyFragmentActivity"
+        self.content = os.popen(cmd)
+
+
+    def stop_app(self):
+        #停止app的命令
+        cmd ="adb shell am force-stop com.woqutz.didi"
+        os.popen(cmd)
+
+    def get_timedata(self):
+        for line in self.content.readlines():
+            if "ThisTime" in line:
+                 thisTime = line.split(":")[1]
+                 break
+        return thisTime
+
+
+if __name__ == "__main__":
+    app = app()
+    #print(app.get_appLaunchTime())
+    app.get_appLaunchTime()
+    thisTime = app.get_timedata()
+    print(thisTime)
